2b9d6a116ee50568afe2bc9817c3b951a72c5202
sinaiflow <spkespke@gmail.com>
2021-02-01 12:59:37 +0200

    implement models

--- views/register.php
+++ views/register.php
@@ -18,7 +18,7 @@
     </div>
     <div class="mb-3">
         <label class="form-label">Password Repeat</label>
-        <input type="password" name="confirm_password" class="form-control">
+        <input type="password" name="passwordConfirm" class="form-control">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
