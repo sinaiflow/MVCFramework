d8d9de52411248d9d279c35492181b78da2ad85d
sinaiflow <spkespke@gmail.com>
2021-02-02 16:30:00 +0200

    restrict register with exist email

--- core/Model.php
+++ core/Model.php
@@ -12,6 +12,7 @@ abstract class Model
     public const RULE_MIN = 'min';
     public const RULE_MAX = 'max';
     public const RULE_MATCH = 'match';
+    public const RULE_UNIQUE = 'unique';
 
     public array $errors = [];
 
@@ -60,10 +61,23 @@ abstract class Model
                 {
                     $this->addError($attribute, self::RULE_MATCH, $rule);
                 }
-
+                if ($ruleName == self::RULE_UNIQUE)
+                {
+                    $className = $rule['class'];
+                    $uniqueAttr = $rule['attribute'] ?? $attribute;
+                    $tableName = $className::tableName();
+                    $statement = Application::$app->db->prepare("SELECT * FROM $tableName WHERE $uniqueAttr = :attr");
+                    $statement->bindValue(":attr", $value);
+                    $statement->execute();
+                    $record = $statement->fetchObject();
+                    if ($record)
+                    {
+                        $this->addError($attribute, self::RULE_UNIQUE,['field' => $attribute]);
+                    }
+                }
             }
         }
-        return empty($this->errorsr);
+        return empty($this->errors);
     }
 
     public function addError(string $attribute,string $rule,$params = [])
@@ -84,6 +98,7 @@ abstract class Model
             self::RULE_MIN => 'Min length of this field must be {min}',
             self::RULE_MAX => 'Min length of this field must be {max}',
             self::RULE_MATCH => 'This field must be the same as {match}',
+            self::RULE_UNIQUE => 'Record with this {field} already exists',
         ];
     }
 
